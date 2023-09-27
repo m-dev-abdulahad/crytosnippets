80ff07d7e26bf4cbbe62d6d916c4c3cedd6c9285
crytominers <146091619+crytominers@users.noreply.github.com>
2023-09-27 19:44:49 +0530

    Update render.js

--- render.js
+++ render.js
@@ -1,13 +1,22 @@
-class MainComponent
-  {
-    constructor()
-    {
-      this.root = document.querySelector("#MainContent");
-    }
-    render($oldNode,$newNode,$parentNode)
-    {
-    }
-    update($oldNode,$newNode,$parentNode)
-    {
+class MainComponent {
+  constructor() {
+    this.root = document.querySelector("#MainContent");
+  }
+
+  // Render method to add new content to the DOM
+  render($newElement) {
+    if (this.root && $newElement) {
+      this.root.appendChild($newElement);
     }
   }
+
+  // Update method to replace existing content with new content
+  update($oldElement, $newElement) {
+    if ($oldElement && $newElement && $oldElement.parentNode) {
+      $oldElement.parentNode.replaceChild($newElement, $oldElement);
+    }
+  }
+}
+
+// Example usage:
+ 
