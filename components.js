a29c997e18106b183a718e42a256bc43bc771e11
Abdul Ahad <146317366+m-dev-abdulahad@users.noreply.github.com>
2023-09-30 14:50:18 +0530

    Update components.js

--- components.js
+++ components.js
@@ -1,10 +1,35 @@
-class VerticalLayout extends MainComponent
-{
-    
-    constrcutor()
-    {
-        this.$verticalLayout =  createRealNode();
+class VerticalLayout extends MainComponent {
+  constructor() {
+    super(); // Call the constructor of the parent class.
+
+    // Create a container virtual node for the vertical layout.
+    this.virtualLayout = {
+      tag: 'div', // Assuming a <div> container for the layout.
+      attributes: {
+        class: 'vertical-layout', // You can add a CSS class for styling.
+      },
+      childNodes: [],
+    };
+
+    // Convert the virtual layout to a real DOM element.
+    this.$verticalLayout = createRealNode(this.virtualLayout);
+  }
+
+  // Method to add a virtual child node to the vertical layout.
+  addChild(virtualChildNode) {
+    if (virtualChildNode) {
+      this.virtualLayout.childNodes.push(virtualChildNode);
+
+      // Update the real DOM element when a child is added.
+      this.update(this.$verticalLayout, createRealNode(this.virtualLayout));
     }
+  }
 
+  // Method to clear all virtual child nodes from the vertical layout.
+  clear() {
+    this.virtualLayout.childNodes = [];
 
+    // Clear the real DOM element when all children are removed.
+    this.update(this.$verticalLayout, createRealNode(this.virtualLayout));
+  }
 }
